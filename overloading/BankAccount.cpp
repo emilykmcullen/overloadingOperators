bd7505f752afcfe1188542cf14572f8c6c167ae8
emilykmcullen <emilycullen135@gmail.com>
2021-04-29 19:36:01 +0100

    added template function

---
+++ overloading/BankAccount.cpp
@@ -0,0 +1,8 @@
+//
+//  BankAccount.cpp
+//  overloading
+//
+//  Created by Emily Cullen on 29/04/2021.
+//
+
+#include "BankAccount.hpp"
--- overloading/main.cpp
+++ overloading/main.cpp
@@ -8,6 +8,12 @@
 #include <iostream>
 #include "Person.hpp"
 
+template <class T>
+T myMax(T const& t1, T const& t2){
+    return t1 < t2 ? t2 : t1;
+    
+};
+
 int main(int argc, const char * argv[]) {
     Person p1("Kate", "Gregory", 123);
     Person p2("Emily", "Cullen", 456);
@@ -29,6 +35,20 @@ int main(int argc, const char * argv[]) {
     if(!(300 < p1))
         std::cout << "not ";
     std::cout << "less than p1" << std::endl;
+    
+    //USING MAX TEMPLATE FUNCTION
+    std::cout << "Max of 33 and 44 is " << myMax(33, 44) << std::endl;
+    
+    std::string s1 = "Hello";
+    std::string s2 = "World";
+    //these will be sorted alphabetically, because 'w' is further
+    //down the alphabet than 'h', world will be bigger
+    std::cout << "Max of hello and world is " << myMax(s1, s2) << std::endl;
+    
+    //this one will use an overloaded operator from
+    //the person class! so it will compare the arbitrary numbers
+    //as we did there
+    std::cout << "Max of person 1 and person 2 is " << myMax(p1, p2).GetName() << std::endl;
     
     return 0;
 }
